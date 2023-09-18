3d9b750fbe0a6cd679179aac3cab908c335aba4e
Oleg OJ <cranberries2000@gmail.com>
2023-09-18 16:42:09 +0300

    Added task 4

--- Program.cs
+++ Program.cs
@@ -3,15 +3,21 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-System.Console.WriteLine("Input two numbers: ");
-int num_1 = Convert.ToInt32(Console.ReadLine());
-int num_2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input two numbers: ");
+// int num_1 = Convert.ToInt32(Console.ReadLine());
+// int num_2 = Convert.ToInt32(Console.ReadLine());
 
-if (num_1 > num_2)
-{
-    Console.WriteLine($"a = {num_1}; b = {num_2} -> max = {num_1}");
-}
-else
-{
-    Console.WriteLine($"a = {num_1}; b = {num_2} -> max = {num_2}");
-}
+// if (num_1 > num_2)
+// {
+//     Console.WriteLine($"a = {num_1}; b = {num_2} -> max = {num_1}");
+// }
+// else
+// {
+//     Console.WriteLine($"a = {num_1}; b = {num_2} -> max = {num_2}");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
