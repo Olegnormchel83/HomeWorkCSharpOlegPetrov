c3c0a069786e072f81826622ef6b6878c885344a
Oleg OJ <cranberries2000@gmail.com>
2023-10-02 15:28:46 +0300

    Task 27 added

--- Program.cs
+++ Program.cs
@@ -240,3 +240,20 @@
 // 82 -> 10
 // 9012 -> 12
 
+int SumNumbersInNumber(int number)
+{
+    int result = 0;
+
+    while (number > 0)
+    {
+        result += number % 10;
+        number / 10;
+    }
+
+    return result;
+}
+
+System.Console.WriteLine("Введите число: ");
+int users_number_1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма цифр в введенном числе -> {SumNumbersInNumber(user_number_1)}");
