b4267dacebfa4d3241b059445fe8c852978c579c
Oleg OJ <cranberries2000@gmail.com>
2023-10-02 15:33:42 +0300

    Task 29 solved

--- Program.cs
+++ Program.cs
@@ -235,25 +235,64 @@
 // System.Console.Write($"Число 1 в степени числа 2 -> {Degree(users_number_1, users_number_2)}");
 
 
+
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
 
-int SumNumbersInNumber(int number)
-{
-    int result = 0;
+// int SumNumbersInNumber(int number)
+// {
+//     int result = 0;
 
-    while (number > 0)
-    {
-        result += number % 10;
-        number /= 10;
-    }
+//     while (number > 0)
+//     {
+//         result += number % 10;
+//         number /= 10;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-System.Console.WriteLine("Введите число: ");
-int users_number_1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число: ");
+// int users_number_1 = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine($"Сумма цифр в введенном числе -> {SumNumbersInNumber(users_number_1)}");
+// System.Console.WriteLine($"Сумма цифр в введенном числе -> {SumNumbersInNumber(users_number_1)}");
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// System.Console.Write("Input size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Input min value: ");
+// int min_value = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Input max value: ");
+// int max_value = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size, min_value, max_value);
+// System.Console.Write("Random array - > ");
+// PrintArray(myArray);
